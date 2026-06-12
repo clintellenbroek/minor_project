73254e91218acfa32584a8dd89398bf80dee2431
siloonk <siloonk@gmail.com>
2026-06-12 10:52:05 +0200

    Start on the Status Effects UI

--- Assets/Scripts/EconomyManager.cs
+++ Assets/Scripts/EconomyManager.cs
@@ -7,6 +7,8 @@ public class EconomyManager : MonoBehaviour
     public int energy = 100;
     public int waterLevel = 100;
 
+    float timeLeft = 0.3f;
+
     public void IncreaseMood(int amount)
     {
         mood += amount;
@@ -20,5 +22,18 @@ public class EconomyManager : MonoBehaviour
     public void IncreaseWaterLevel(int amount)
     {
         waterLevel += amount;
+    }
+
+
+    private void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft <= 0)
+        {
+            mood -= 5;
+            energy -= 5;
+            timeLeft = .3f;
+        }
     }
 }
---
+++ Assets/Scripts/SliderHandler.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class SliderHandler : MonoBehaviour
+{
+    [Header("Slider Object")]
+    public Slider slider;
+
+    public EconomyManager economyManager;
+    public String propertyName;
+
+
+    private FieldInfo field;
+
+    private void Awake()
+    {
+        field = economyManager.GetType().GetField("mood");
+    }
+
+    public void Update()
+    {
+        slider.value = (int)field.GetValue(economyManager);
+    }
+}
