d154cb34fdac8b506837fe0dcdbca61d78147ade
siloonk <siloonk@gmail.com>
2026-06-24 18:24:15 +0200

    Fix main menu, tweak some player settings

--- Assets/Scripts/MainScreen.cs
+++ Assets/Scripts/MainScreen.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("Game");
     }
 
     public void OpenOptions()
